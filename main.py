36405952a9aafc62bf10d80cd7b5a352815bbea2
scox1502 <samuelc506@deltalearns.ca>
2025-03-03 17:40:22 +0000

    finished "pets"

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 pets = []
+people = []
+citys = []
 
 pet1 = {
 	'type of animal':'cat',
@@ -7,7 +9,19 @@ pet1 = {
 pets.append(pet1)
 
 pet2 = {
-    'type of animal':'',
-}
+    'type of animal':'cat',
+	'name':'timbit',
+	'owner':'troy', }
+pets.append(pet2)
 
+pet3 = {
+	'type of animal':'hamster',
+	'name':'vera',
+	'owner':'mickeal', }
+pets.append(pet3)
 print(f"{pets}")
+
+people1 = {
+	
+
+}
